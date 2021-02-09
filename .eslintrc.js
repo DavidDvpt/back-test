26b3249660fa999e15d84dae277f30d9b4b3e959
David Mosca <david.mosca69@gmail.com>
2021-02-09 22:44:11 +0100

    add prettier and config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,16 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "no-unused-vars": "warn",
   },
 };
